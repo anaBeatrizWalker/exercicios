b6e59fe41f171be7b69f0bccd7f28ff135aaf9ee
Ana Beatriz <anabiawalker@gmail.com>
2022-01-13 15:47:00 -0300

    Objeto global do node

---
+++ Node/ex04_global.js
@@ -0,0 +1,4 @@
+const pi = 3.14
+console.log(global.pi) //undefined
+
+global.obj = {name: 'Estou no global'}
---
+++ Node/ex04_test.js
@@ -0,0 +1,4 @@
+require('./ex04_global')
+
+console.log(global.obj.name) //Estou no global
+console.log(obj.name) //Estou no global
